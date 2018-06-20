ba4c915a430322bfc75eaddf40bc1c71a27696e9
Rafael Meza <rhmcord@gmail.com>
2018-06-20 17:29:09 -0500

    Improve the ttt game

--- ttt.rb
+++ ttt.rb
@@ -1,50 +1,47 @@
+@values = %w(X O)
+
 def board
-  @positions = {
-    'a1' => ' ',
-    'a2' => ' ',
-    'a3' => ' ',
-    'b1' => ' ',
-    'b2' => ' ',
-    'b3' => ' ',
-    'c1' => ' ',
-    'c2' => ' ',
-    'c3' => ' '
-  }
+  @positions = {}
+  tiles = %i(a1 a2 a3 b1 b2 b3 c1 c2 c3)
+  tiles.each { |item| @positions[item] = ' ' }
 end
 
 def wins
   [
-    [@positions['a1'], @positions['a2'], @positions['a3']],
-    [@positions['b1'], @positions['b2'], @positions['b3']],
-    [@positions['c1'], @positions['c2'], @positions['c3']],
-    [@positions['a1'], @positions['b1'], @positions['c1']],
-    [@positions['a2'], @positions['b2'], @positions['c2']],
-    [@positions['a3'], @positions['b3'], @positions['c3']],
-    [@positions['a1'], @positions['b2'], @positions['c3']],
-    [@positions['a3'], @positions['b2'], @positions['c1']]
-  ]
+    %i(a1 a2 a3),
+    %i(b1 b2 b3),
+    %i(c1 c2 c3),
+    %i(a1 b1 c1),
+    %i(a2 b2 c2),
+    %i(a3 b3 c3),
+    %i(a1 b2 c3),
+    %i(a3 b2 c1)
+  ].map do |list|
+    list.map { |item| @positions[item] }
+  end
+end
+
+def draw_line(list)
+  list.map { |item| @positions[item] }.join(' | ')
 end
 
 def draw_board
   puts
   puts "   1   2   3"
-  puts "a  #{@positions['a1']} | #{@positions['a2']} | #{@positions['a3']} "
+  puts "a  #{draw_line(%i(a1 a2 a3))} "
   puts "  ---|---|---"
-  puts "b  #{@positions['b1']} | #{@positions['b2']} | #{@positions['b3']} "
+  puts "b  #{draw_line(%i(b1 b2 b3))} "
   puts "  ---|---|---"
-  puts "c  #{@positions['c1']} | #{@positions['c2']} | #{@positions['c3']} "
+  puts "c  #{draw_line(%i(c1 c2 c3))} "
   puts
 end
 
 def check_new_choice(choice)
-  if choice == 'q'
-    puts 'Bye!'
-    exit
-  end
+  abort('Bye!') if choice == 'q'
 end
 
 def same_values(values)
-  values.uniq.length == 1
+  values.uniq.size == 1
 end
 
 def available_squares
@@ -53,59 +50,31 @@ end
 
 def check_for_winner
   wins.each do |win|
-    if same_values(win) && any_empty_values(win)
-      puts 'Win!!!'
-      exit
-    end
-    if same_values(win) && !available_squares
-      puts 'Win!!!'
-      exit
-    end
-    if !same_values(win) && !available_squares
-      puts 'Draw!!!'
-      exit
-    end
+    abort('Win!!!')  if same_values(win)  && any_empty_values(win)
+    abort('Win!!!')  if same_values(win)  && !available_squares
+    abort('Draw :/') if !same_values(win) && !available_squares
   end
 end
 
-def any_empty_values(arr)
-  (arr & ['X', 'O']).any?
+def any_empty_values(list)
+  (list & @values).any?
 end
 
-def turn_X
+def turn(value)
   check_for_winner
-  puts 'Turn (X), please choose a square:'
-  new_choice = gets.chomp.downcase
+  puts "Turn (#{value}), please choose a square:"
+  new_choice = gets.chomp.downcase.to_sym
   check_new_choice(new_choice)
   @positions.each do |choice, square|
-    if new_choice == choice
-      if square == ' '
-        @positions[choice] = 'X'
-        draw_board
-        turn_O
-      else
-        draw_board
-        turn_X
-      end
-    end
-  end
-end
-
-def turn_O
-  check_for_winner
-  puts 'Turn (O), please choose a square:'
-  new_choice = gets.chomp.downcase
-  check_new_choice(new_choice)
-  @positions.each do |choice, square|
-    if new_choice == choice
-      if square == ' '
-        @positions[choice] = 'O'
-        draw_board
-        turn_X
-      else
-        draw_board
-        turn_O
-      end
+    next unless new_choice == choice
+    if square.strip.empty?
+      @positions[choice] = value
+      draw_board
+      next_value = (@values - [value]).first
+      turn(next_value)
+    else
+      draw_board
+      turn(value)
     end
   end
 end
@@ -116,7 +85,7 @@ def init
   while true
     check_for_winner
     draw_board
-    turn_X
+    turn(@values.first)
   end
 end
 
